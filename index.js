4b9aed8c5e4f90f8e0860cea7fd5972dab9c300c
Aaron Blum <blumaa@gmail.com>
2019-07-18 16:01:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,5 +63,6 @@ function placeOrder(cardNumber) {
         return("Sorry, we don't have a credit card on file for you.")
   }else {
     sum_statement = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = []
   }
 }
