a8c8dd2afd048ae6f9de4c7d7b7ada28b1c5b457
Aaron Blum <blumaa@gmail.com>
2019-07-18 16:06:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var sum_statement = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  if (cardNumber[0] = 'undefined') {
+  if (cardNumber = 'undefined') {
         return("Sorry, we don't have a credit card on file for you.")
   }else {
     sum_statement = (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
